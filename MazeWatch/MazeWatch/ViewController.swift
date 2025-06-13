ae5437c1016404156a069f316bd03a55267736ca
Yago Vanzan <villarvanzan@gmail.com>
2025-06-13 18:02:35 -0300

    init project

---
+++ MazeWatch/MazeWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazeWatch
+//
+//  Created by Yago Vanzan on 13/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
