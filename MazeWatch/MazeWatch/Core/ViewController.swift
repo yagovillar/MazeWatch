501fc56b8584821e6608910229f4ccf77ccc1414
Yago Vanzan <villarvanzan@gmail.com>
2025-06-13 18:55:31 -0300

    Files organization

---
+++ MazeWatch/MazeWatch/Core/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MazeWatch
+//
+//  Created by Yago Vanzan on 13/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
